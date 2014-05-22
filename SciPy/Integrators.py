2aa820beb682222e3dc3bef9f3bbc73652a8b987
Mary Naworski <mnaworsk@calpoly.edu>
2014-05-22 15:16:04 -0700

    integration exercises error handled and began to work through RMC tour

--- SciPy/Integrators.py
+++ SciPy/Integrators.py
@@ -1,23 +1,22 @@
 
-def trapz(func, a, b, n): 
+import numpy as np
+
+def trapz(func, a, b, N): 
     """this function claculates an integral by approximating the area with
     trapezoidal pieces""" 
-    a = 0.0
-    b = 2.0
     h = (b-a)/N
     
     k = np.arange(1,N)
     I = h*(0.5*func(a) + 0.5*func(b) + func(a+k*h).sum())
+    return I
 
-def simps(func, a, b, n): 
+def simps(func, a, b, N): 
     """this function calculates an integral with quadratic curves. 
     It is a closer approximation that the trapezoidal rule."""
-    a = 0.0
-    b = 2.0
     h = (b-a)/N
-
+    
     k1 = np.arange(1,N/2+1)
     k2 = np.arange(1,N/2)
-
-
-print file.name
+    I = I = (1./3.)*h*(func(a) + func(b) + 4.*func(a+(2*k1-1)*h).sum() + 2.*func(a+2*k2*h).sum())
+    
+    return I
